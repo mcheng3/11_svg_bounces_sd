6d749d652fa54f9df3ee6948cdb9a9b92748c4a0
Max Korsun <mkorsun@stuy.edu>
2018-03-11 23:19:41 -0400

    done

--- vector.js
+++ vector.js
@@ -2,46 +2,72 @@ var pic = document.getElementById("pic");
 var hasMadeFirst = false;
 var id;
 
-var animateDVD = function(e){
-    cancel();
-    var x = e.offsetX;
-    var y = e.offsetY;
-    console.log(x);
-    console.log(y);
-    var drawDVD = function(){
-	clear();
-	var c = document.createElementNS( "http://www.w3.org/2000/svg","circle");
-	c.setAttribute("cx", x);
-	c.setAttribute("cy", y);
-	c.setAttribute("r", 50);
-	c.setAttribute("xVel", 1.2);
-	c.setAttribute("yVel", 1.2);
-	x+=parseFloat(c.getAttribute("xVel"));
-	y+=parseFloat(c.getAttribute("yVel"));
-	if(x < 50 || x > 700){
-	    console.log(parseFloat(c.getAttribute("xVel")) * -1);
-	    c.setAttribute("xVel", parseFloat(c.getAttribute("xVel")) * -1);
-	}
-	if(y < 50 || y > 450){
-	    c.setAttribute("yVel", parseFloat(c.getAttribute("yVel")) * -1);
-	}
-	c.setAttribute("fill", "lightsteelblue");
-	pic.appendChild(c);
-    };
-    id = window.setInterval(drawDVD, 10);
-};
-
-
 var clear = function(){
-    //window.clearInterval(id);
-	pic.innerHTML = "";
-	hasMadeFirst = false;
-};
-
-var cancel = function(){
-    window.clearInterval(id);
-    clear();
+    while(pic.firstChild){
+	pic.removeChild(pic.firstChild);
+    }
+    clearInterval(id);
 }
 
-document.getElementById("clear").addEventListener("click", cancel);
-pic.addEventListener("click", animateDVD);
+var circle = function(e) {
+    clearInterval(id);
+    var c = document.createElementNS( "http://www.w3.org/2000/svg","circle");
+    c.setAttribute("cx", e.offsetX + "");
+    c.setAttribute("cy", e.offsetY + "");
+    c.setAttribute("r", 50+"");
+    c.setAttribute("xvel", 1);
+    c.setAttribute("yvel",1);
+    c.setAttribute("fill","lightsteelblue");
+    c.setAttribute("stroke","lightsteelblue");
+    pic.append(c);
+}
+    
+
+
+var bouncehelp= function(){
+    var x,y,xvel,yvel;
+    for (var i = 0; i < pic.children.length; i++){
+	var c = pic.children[i];
+	x=Number(c.getAttribute("cx"));
+	y=Number(c.getAttribute("cy"));
+	xvel=Number(c.getAttribute("xvel"));
+	yvel = Number(c.getAttribute("yvel"));
+	c.setAttribute("cx", x+xvel+"");
+	c.setAttribute("cy", y+yvel+"");
+	if(x <= 50){
+	    c.setAttribute("xvel", 1);
+	}
+	if (x >= 650){
+	    c.setAttribute("xvel",0);
+	}
+	if(y <= 50){
+	    c.setAttribute("yvel", 1);
+	}
+	if (y >= 450){
+	    c.setAttribute("yvel",0);
+	}
+	if (xvel == 1){
+	    c.setAttribute("cx", x+5+"");
+	}
+	else{
+	    c.setAttribute("cx", x-5+"")
+	}
+	if (yvel == 1){
+	    c.setAttribute("cy", y+5+"");
+	}
+	else{
+	    c.setAttribute("cy", y-5+"")
+	}
+    }
+}
+
+
+
+var play = function(e){
+    circle(e);
+    id = setInterval(bouncehelp, 10);
+}
+
+
+document.getElementById("clear").addEventListener("click", clear);
+pic.addEventListener("click", play);
